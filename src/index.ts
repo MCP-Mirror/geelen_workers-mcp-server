96fc895c8e61166869122adcddb3f95b56797887
Glen Maddern <glen@cloudflare.com>
2024-12-06 16:12:28 +1100

    ready to record demo

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,28 @@ export default class ExampleWorkerMCP extends WorkerEntrypoint<Env> {
   }
 
   /**
+   * Takes a screenshot of a given URL.
+   *
+   * @param {string} url The URL to take a screenshot of
+   * @return {Promise<Response>} A Response with an image/png type body containing the screenshot
+   *
+   * @example
+   * const response = await worker.takeScreenshot('https://example.com')
+   * const img = await response.arrayBuffer()
+   */
+  async takeScreenshot(url: string) {
+    const browser = await puppeteer.launch(this.env.MYBROWSER)
+    const page = await browser.newPage()
+    await page.setViewport({ width: 768, height: 1024 })
+    await page.goto(url)
+
+    const img = await page.screenshot()
+    await browser.close()
+
+    return new Response(img, { headers: { 'Content-Type': 'image/png' } })
+  }
+
+  /**
    * Send a text or HTML email to an arbitrary recipient.
    *
    * @param {string} recipient - The email address of the recipient.
@@ -30,16 +52,10 @@ export default class ExampleWorkerMCP extends WorkerEntrypoint<Env> {
    * @throws {Error} If the email fails to send, or if that destination email address hasn't been verified.
    */
   async sendEmail(recipient: string, subject: string, contentType: string, body: string) {
-    // For users cloning this project who don't have Email Routing enabled:
-    if (!this.env.EMAIL) throw new Error(`'EMAIL' binding not present. Have you added it to wrangler.json?`)
-
     try {
       const msg = createMimeMessage()
       const from = this.env.EMAIL_FROM
 
-      if (from === 'PLEASE CONFIGURE ME') {
-        throw new Error(`Please configure the EMAIL_FROM variable in wrangler.json`)
-      }
       msg.setSender({ name: 'Claude Desktop', addr: from })
       msg.setRecipient(recipient)
       msg.setSubject(subject)
@@ -54,40 +70,6 @@ export default class ExampleWorkerMCP extends WorkerEntrypoint<Env> {
       console.error(error)
       throw error
     }
-  }
-
-  /**
-   * Takes a screenshot of a given URL.
-   *
-   * @param {string} url The URL to take a screenshot of
-   * @return {Promise<Response>} A Response with an image/png type body containing the screenshot
-   *
-   * @example
-   * const response = await worker.takeScreenshot('https://example.com')
-   * const img = await response.arrayBuffer()
-   */
-  async takeScreenshot(url: string) {
-    // const time = Math.floor(+new Date() / 100000) // Changes every 100 seconds
-    // const cacheKey = JSON.stringify({ url, time })
-    //   .replace(/\W+/g, '_')
-    //   .replace(/^_|_$|(?<=_)_+/, '_')
-
-    // let img: ArrayBuffer | null = null
-    // img = await this.env.KV.get(cacheKey, { type: 'arrayBuffer' })
-    // if (img === null) {
-    const browser = await puppeteer.launch(this.env.MYBROWSER)
-    const page = await browser.newPage()
-    await page.setViewport({ width: 768, height: 1024 })
-    console.log(`Going to ${url}.`)
-    // console.log(`Going to ${url}. Cache key ${cacheKey}`)
-    await page.goto(url)
-
-    const img = await page.screenshot()
-    // await this.env.KV.put(cacheKey, img, { expirationTtl})
-    await browser.close()
-    // }
-
-    return new Response(img, { headers: { 'Content-Type': 'image/png' } })
   }
 
   /**
